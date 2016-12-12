f527ab2e949747b4dfbf6b30ac4af657d01bbe06
Mikhail Tamashuk <hover024@gmail.com>
2016-12-12 19:51:41 +0300

    maps

--- credit/test.js
+++ credit/test.js
@@ -9,7 +9,9 @@ const
     fine = new Set(),
     max = new Set(),
     time = new Set(),
-    objectAssign = require('object-assign');
+    { maxSumm } = require('../maps/creditMaxSumm'),
+    objectAssign = require('object-assign'),
+    numeral = require('numeral');
 
 let 
 	fields = {};
@@ -23,26 +25,28 @@ const getSetOfFields = async(function() {
 
 		fields = objectAssign(fields, details);
 
-		payType.add(details['Тип выплат']);
+		payType.add(details['Цель кредита']);
 	    givingType.add(details['Варианты выдачи']);
 	    fine.add(details['Штраф за досрочное погашение кредита']);
-	    max.add(details['Максимально возможная сумма по кредиту']);
+	    max.add(maxSumm[details['Максимально возможная сумма по кредиту']]);
 	    time.add(details['Срок рассмотрения заявки']);
 	}
 });
 
+const maxs = [];
+
 getSetOfFields()
 	.then(() => {
 		console.log(fields);
-		/*console.log('------------payType-------------');
-		console.log(payType);
-		console.log('------------givingType-------------');
-		console.log(givingType);
-		console.log('------------fine-------------');
-		console.log(fine);
-		console.log('------------max-------------');
-		console.log(max);
-		console.log('------------time-------------');
-		console.log(time);*/
+		//console.log('------------payType-------------');
+		//console.log(payType);
+		//console.log('------------givingType-------------');
+		//console.log(givingType);
+		//console.log('------------fine-------------');
+		//console.log(fine);
+		//console.log('------------max-------------');
+		//console.log(max);
+		//console.log('------------time-------------');
+		//console.log(time);*/
 	});
 
